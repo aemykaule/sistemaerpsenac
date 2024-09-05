3e2e501dafd8c4588e5e63d873175c752d57afff
LUIS GUSTAVO NESPOLO SALVADOR <salvador.6916@aluno.pr.senac.br>
2024-09-05 16:41:56 -0300

    P.I. 2024

---
+++ src/js/ScriptData.js
@@ -0,0 +1,9 @@
+new DataTable('#example', {
+    colReorder: true,
+    responsive: true,
+    language:{
+        search:         "Buscar:",
+        lengthMenu:     "_MENU_ elementos por página",
+        info:           "Mostrando _START_ a _END_ de _TOTAL_ entradas",
+    }
+});
